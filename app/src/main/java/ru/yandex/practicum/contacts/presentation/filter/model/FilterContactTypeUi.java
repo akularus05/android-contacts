03cd25d665ba2ccf8a0a851d7947cabff85cebc8
Kirill <akularus05@mail.ru>
2023-05-26 20:06:58 +0300

    проерочная номер 2

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/model/FilterContactTypeUi.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/model/FilterContactTypeUi.java
@@ -2,7 +2,10 @@ package ru.yandex.practicum.contacts.presentation.filter.model;
 
 import androidx.annotation.NonNull;
 
-public class FilterContactTypeUi {
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
+import ru.yandex.practicum.contacts.presentation.main.ContactUi;
+
+public class FilterContactTypeUi implements ListDiffInterface <FilterContactTypeUi> {
 
     private final FilterContactType contactType;
     private final boolean selected;
@@ -19,6 +22,10 @@ public class FilterContactTypeUi {
     public boolean isSelected() {
         return selected;
     }
+    public boolean theSameAs(FilterContactTypeUi object){
+        return this.hashCode() == object.hashCode();
+    }
+
 
     @Override
     public boolean equals(Object o) {
